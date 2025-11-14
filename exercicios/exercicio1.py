80614684cf99f0ee8e1f90762bfcd63e3e885b4e
Adilson <adilsonchy@gmail.com>
2025-11-14 12:31:39 -0300

    exercicio de lógica
    fiz alguns exercicios relacionados a lógica booleana

---
+++ exercicios/exercicio1.py
@@ -0,0 +1,62 @@
+
+
+# leia A e exiba NOT A:
+
+# A = True
+
+# print(f"not A = {not A}")
+
+#################
+
+#leia A e B e calcule A AND B
+
+# A = True
+# B = True
+
+# print(f"A = True AND B = True | {A and B}")
+# print(f"A = False AND B = True | {not A and B}")
+# print(f"A = True AND B = False | { A and not B}")
+# print(f"A = False AND B = False | {not A and not B}")
+
+########################
+
+#leia A e B e calcule A AND B
+
+# A = True
+# B = True
+
+# print(f"A = True OR B = True | {A or B}")
+# print(f"A = False OR B = True | {not A or B}")
+# print(f"A = True OR B = False | {A or not B}")
+# print(f"A = False OR B = False | {not A or not B}")
+
+######################
+
+#chovendo=true e frio=false, mostre as combinações
+#combinações de portas lógicas ou resultados????
+chovendo = True
+frio = False
+
+print("\n POSSIBILIDADES COM AND: \n")
+print(f" chovendo = true AND NOT frio = false | {chovendo and not frio}")
+print(f" NOT chovendo = true AND  frio = false | {not chovendo and frio}")
+print(f" NOT chovendo = true AND NOT frio = false | {not chovendo and not frio}")
+print(f" chovendo = true AND  frio = false | {chovendo and frio} \n \n")
+
+
+print("\n POSSIBILIDADES COM OR: \n")
+print(f" chovendo = true OR NOT frio = false | {chovendo or not frio}")
+print(f" NOT chovendo = true OR  frio = false | {not chovendo or frio}")
+print(f" NOT chovendo = true OR NOT frio = false | {not chovendo or not frio}")
+print(f" chovendo = true OR  frio = false | {chovendo or frio} \n \n")
+
+
+
+
+
+
+
+
+
+
+
